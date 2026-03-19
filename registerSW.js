8357fd3fdb4c665ee6ce55763349d81aefd44587
tarako617 <tarako617@users.noreply.github.com>
2026-03-19 18:28:31 +0000

    deploy: 28838f22bc260bfdded3bd8140c14655b60ddf6d

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/mobile-plan-simulator/sw.js', { scope: '/mobile-plan-simulator/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
